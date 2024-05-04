bbadbeb61aa690853e967d534fa39ba7f92514ae
AyahNasser <122571192+AyahNasser@users.noreply.github.com>
2024-05-04 00:35:52 -0700

    update

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,33 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  const airhorn = document.getElementById('horn-select');
+  const image = document.querySelector('img');
+  image.src = ('assets/images/no-image.png');
+
+  airhorn.addEventListener('change', function() {
+
+    if(airhorn.value == 'select'){
+      image.src =('assets/images/no-image.png');
+
+    }
+    else if(airhorn.value == 'air-horn'){
+    image.src =('assets/images/air-horn.svg');
+    } 
+    else if(airhorn.value == 'car-horn'){
+      image.src =('assets/images/car-horn.svg');
+
+    }
+    else if (airhorn.value == 'party-horn'){
+      image.src =('assets/images/party-horn.svg');
+
+    }
+    else {
+      image.src =('assets/images/no-image.png');
+    }
+    // code to run when the event is triggered
+  });
+
+
 }
